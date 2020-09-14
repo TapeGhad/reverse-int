0dd24907d32be1d035184eb7eb4158689bc314c9
tapeghad <tapeghad@gmail.com>
2020-09-14 15:10:49 +0300

    feat: finish 20/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let arr = [];
+  while (n > 0) {
+      arr.push(n % 10);
+      n = Math.floor(n / 10);
+  }
+  return arr.reduce((final, elem, index) => {
+      return final = final + elem * Math.pow(10, arr.length - 1 - index);
+  }, 0)
 }
